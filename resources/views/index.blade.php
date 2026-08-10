bdbe97401ccab0251a819b76592cd1384c71f2d3
Raj Masihi <rajmasihi6397@gmail.com>
2026-08-10 23:41:09 +0530

    Merge remote  and css update 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
     @endphp
     <div class="container-fluid main-section" id="cricket-index-page" data-active-tab="{{ e($activeTab) }}">
 <<<<<<< HEAD
-        <h4 class="text-center mb-3">Cricket Live Scores</h4>
+        <h4 class="text-center mb-3">Cricket Live Score</h4>
 =======
         <h4 class="text-center mb-3">Cricket Live Score</h4>
 >>>>>>> origin/main
